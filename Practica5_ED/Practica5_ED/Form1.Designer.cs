003ad69e5afc053b9b14ab0231f0f81117602604
AlejandroMartinezRubio <alemarrub@alu.edu.gva.es>
2023-11-24 18:47:55 +0100

    Parte grafica hecha

--- Practica5_ED/Practica5_ED/Form1.Designer.cs
+++ Practica5_ED/Practica5_ED/Form1.Designer.cs
@@ -28,55 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lResultado = new System.Windows.Forms.Label();
-            this.bCalcular = new System.Windows.Forms.Button();
-            this.tCantidad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbUrgente = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // lResultado
+            // button1
             // 
-            this.lResultado.AutoSize = true;
-            this.lResultado.Location = new System.Drawing.Point(116, 130);
-            this.lResultado.Name = "lResultado";
-            this.lResultado.Size = new System.Drawing.Size(0, 13);
-            this.lResultado.TabIndex = 7;
+            this.button1.Location = new System.Drawing.Point(464, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 41);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // bCalcular
+            // txtPrecio
             // 
-            this.bCalcular.Location = new System.Drawing.Point(389, 53);
-            this.bCalcular.Name = "bCalcular";
-            this.bCalcular.Size = new System.Drawing.Size(75, 23);
-            this.bCalcular.TabIndex = 6;
-            this.bCalcular.Text = "Calcular";
-            this.bCalcular.UseVisualStyleBackColor = true;
-            this.bCalcular.Click += new System.EventHandler(this.bCalcular_Click);
+            this.txtPrecio.Location = new System.Drawing.Point(93, 301);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 10;
             // 
-            // tCantidad
+            // label2
             // 
-            this.tCantidad.Location = new System.Drawing.Point(116, 64);
-            this.tCantidad.Name = "tCantidad";
-            this.tCantidad.Size = new System.Drawing.Size(100, 20);
-            this.tCantidad.TabIndex = 5;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Coste:";
+            // 
+            // cbUrgente
+            // 
+            this.cbUrgente.AutoSize = true;
+            this.cbUrgente.Location = new System.Drawing.Point(50, 252);
+            this.cbUrgente.Name = "cbUrgente";
+            this.cbUrgente.Size = new System.Drawing.Size(70, 17);
+            this.cbUrgente.TabIndex = 8;
+            this.cbUrgente.Text = "Urgente?";
+            this.cbUrgente.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 64);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Valor:";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Texto";
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(50, 65);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(515, 152);
+            this.txtTelegrama.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lResultado);
-            this.Controls.Add(this.bCalcular);
-            this.Controls.Add(this.tCantidad);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbUrgente);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTelegrama);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -86,10 +110,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lResultado;
-        private System.Windows.Forms.Button bCalcular;
-        private System.Windows.Forms.TextBox tCantidad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbUrgente;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTelegrama;
     }
 }
 
