8d73c5888d2b0875805f0bef1e74612cd1678587
gimenezjuan <gimenezcorbalan@gmail.com>
2023-11-24 19:53:14 +0100

    Codigo corregido y terminado

--- Practica5_ED/Practica5_ED/Form1.cs
+++ Practica5_ED/Practica5_ED/Form1.cs
@@ -19,33 +19,57 @@ namespace Practica5_ED
 
         private void bCalcular_Click(object sender, EventArgs e)
         {
-                string textoTelegrama;
-                char tipoTelegrama = ' ';
-                int numPalabras = 0;
-                double coste;
-                //Leo el telegrama
-                textoTelegrama = txtTelegrama.Text;
-                // telegrama urgente?
-                if (cbUrgente.Checked)
-                    tipoTelegrama = 'u';
-                //Obtengo el número de palabras que forma el telegrama
-                numPalabras = textoTelegrama.Length;
-                //Si el telegrama es ordinario
-                if (tipoTelegrama == 'o')
-                    if (numPalabras <= 10)
-                        coste = 25;
-                    else
-                        coste = 0.5 * numPalabras;
+            string textoTelegrama;
+            char tipoTelegrama = ' ';
+            int numPalabras = 0;
+            double coste = 0;
+
+            // Leo el telegrama
+            textoTelegrama = txtTelegrama.Text;
+
+            // Telegrama urgente?
+            if (cbUrgente.Checked)
+            {
+                tipoTelegrama = 'u';
+            }
+
+            else
+            {
+                tipoTelegrama = 'o';
+            }
+
+
+            // Obtengo el número de palabras que forma el telegrama
+            numPalabras = textoTelegrama.Split().Length;
+
+            // Calcular el coste del telegrama
+            if (tipoTelegrama == 'o')
+            {
+                if (numPalabras <= 10)
+                {
+                    coste = 2.5;
+                }
                 else
-                //Si el telegrama es urgente
-                if (tipoTelegrama == 'u')
-                    if (numPalabras <= 10)
-                        coste = 5;
-                    else
-                        coste = 5 + 0.75 * (numPalabras - 10);
+                {
+                    coste = 2.5 + 0.5 * (numPalabras - 10);
+                }
+            }
+            else if (tipoTelegrama == 'u')
+            {
+                if (numPalabras <= 10)
+                {
+                    coste = 5;
+                }
                 else
-                    coste = 0;
-                txtPrecio.Text = coste.ToString() + " euros";
+                {
+                    coste = 5 + 0.75 * (numPalabras - 10);
+                }
+            }
+
+            // Mostrar el coste en el campo txtPrecio
+            //txtPrecio.Text = coste.ToString("C [ABB]"); // "C" formatea el número como moneda
+            txtPrecio.Text = coste.ToString("C");
+
         }
     }
 }
