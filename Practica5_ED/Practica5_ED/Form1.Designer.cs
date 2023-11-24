f97ae1bb9469cbbb7eba6e1c1aa1d7180193d122
AlejandroMartinezRubio <alemarrub@alu.edu.gva.es>
2023-11-24 16:59:04 +0100

    Diseño Grafico del Form hecho

--- Practica5_ED/Practica5_ED/Form1.Designer.cs
+++ Practica5_ED/Practica5_ED/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lResultado = new System.Windows.Forms.Label();
+            this.bCalcular = new System.Windows.Forms.Button();
+            this.tCantidad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lResultado
+            // 
+            this.lResultado.AutoSize = true;
+            this.lResultado.Location = new System.Drawing.Point(116, 130);
+            this.lResultado.Name = "lResultado";
+            this.lResultado.Size = new System.Drawing.Size(0, 13);
+            this.lResultado.TabIndex = 7;
+            // 
+            // bCalcular
+            // 
+            this.bCalcular.Location = new System.Drawing.Point(389, 53);
+            this.bCalcular.Name = "bCalcular";
+            this.bCalcular.Size = new System.Drawing.Size(75, 23);
+            this.bCalcular.TabIndex = 6;
+            this.bCalcular.Text = "Calcular";
+            this.bCalcular.UseVisualStyleBackColor = true;
+            // 
+            // tCantidad
+            // 
+            this.tCantidad.Location = new System.Drawing.Point(116, 64);
+            this.tCantidad.Name = "tCantidad";
+            this.tCantidad.Size = new System.Drawing.Size(100, 20);
+            this.tCantidad.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Valor:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lResultado);
+            this.Controls.Add(this.bCalcular);
+            this.Controls.Add(this.tCantidad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lResultado;
+        private System.Windows.Forms.Button bCalcular;
+        private System.Windows.Forms.TextBox tCantidad;
+        private System.Windows.Forms.Label label1;
     }
 }
 
